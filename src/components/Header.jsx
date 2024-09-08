b5cc76f8dc3be8b786cb1ab7f86f3d0a9d344e2b
Murad Balayev <muradbalazade@gmail.com>
2024-09-08 11:33:54 +0400

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { FaAppStoreIos } from "react-icons/fa";
 import { BsGooglePlay } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 
 
@@ -15,59 +16,74 @@ import mobileapp from '../assets/mobileapp.png'
 
 const Header = () => {
     return (
-        <header>
-            <nav className="w-full flex gap-12 justify-between items-center ">
+        <motion.header
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}>
+            <motion.nav
+             initial={{  y: -20 }}
+             animate={{  y: 0 }}
+             transition={{ duration: 0.6, ease: "easeOut" }}
+             className="w-full flex gap-12 justify-between items-center ">
                 <div className="logo flex items-center md:min-w-60 sm:min-w-40 sm:max-w-60 max-w-44 min-w-36 md:max-w-72">
                     <img alt="logo" className="object-contain" src={logo} />
                 </div>
                 <div className="socials flex md:gap-3 gap-2  items-center">
                     <a className="btn-pink">
-                        <FaFacebookF  className="sm:w-6 sm:h-6"/>
+                        <FaFacebookF className="sm:w-6 sm:h-6" />
                     </a>
                     <a className="btn-pink">
-                        <RiInstagramFill className="sm:w-6 sm:h-6"/>
+                        <RiInstagramFill className="sm:w-6 sm:h-6" />
                     </a>
                     <a className="btn-pink">
-                        <PiTiktokLogoFill  className="sm:w-6 sm:h-6"/>
+                        <PiTiktokLogoFill className="sm:w-6 sm:h-6" />
                     </a>
                     <a className="btn-pink">
-                        <FaLinkedinIn  className="sm:w-6 sm:h-6" />
+                        <FaLinkedinIn className="sm:w-6 sm:h-6" />
                     </a>
                 </div>
-            </nav>
+            </motion.nav>
             <div className="h-full grid sm:grid-cols-2 sm:pt-[30px] pt-12">
                 <div className="section__left flex flex-col justify-center">
-                    <h1 className="font-extrabold sm:max-w-[550px] sm:mb-16 mb-10 sm:text-left text-center mx-auto max-w-[300px]">Hər şey bir fincan qəhvə ilə başlayır!</h1>
+                    <motion.h1
+                        initial={{ opacity: 0, x: -50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 1, ease: "easeOut" }}
+                        className="font-extrabold sm:max-w-[550px] sm:mb-16 mb-10 sm:text-left text-center mx-auto max-w-[300px]">Hər şey bir fincan qəhvə ilə başlayır!</motion.h1>
                     <div className="sm:grid hidden sm:grid-cols-2 grid-cols-1 gap-3 max-w-96 mx-auto mb-4">
-                        <a className="px-3 py-2 border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><FaAppStoreIos className="w-6 h-6"  color="blue" /> App Store</a>
-                        <a className="px-3 py-2  border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><BsGooglePlay className="w-6 h-6"  color="blue" /> Google Play</a>
+                        <a className="px-3 py-2 border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><FaAppStoreIos className="w-6 h-6" color="blue" /> App Store</a>
+                        <a className="px-3 py-2  border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><BsGooglePlay className="w-6 h-6" color="blue" /> Google Play</a>
                     </div>
                 </div>
-                <div className="section__right relative sm:flex-row flex-col flex justify-center items-start px-5 pb-16 w-full mx-auto">
-                   <div className="relative sm:pb-12 pb-20 sm:px-0 px-5">
-                    <img src={mobileapp} className="phone-img object-fit" alt="mobile-app" />
-                    <div className="absolute sm:bottom-0 bottom-5 left-14 rounded-2xl md:px-4 lg:py-4 px-3 py-2 flex-col bg-white lg:max-w-60 md:max-w-44 sm:max-w-36 max-w-44">
-                        <div className="flex sm:gap-2 gap-1 lg:mb-3 sm:mb-1 mb-2">
-                            <FaStar className="w-4"/>
-                            <FaStar className="w-4 "/>
-                            <FaStar className="w-4 "/>
-                            <FaStar className="w-4"/>
-                            <FaStar className="w-4 "/>
+                <motion.div
+                    initial={{ x: 50 }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: 0.8, ease: "easeOut" }}
+                    className="section__right relative sm:flex-row flex-col flex justify-center items-start px-5 pb-16 w-full mx-auto">
+                    <div className="relative sm:pb-12 pb-20 sm:px-0 px-5">
+                        <img src={mobileapp} className="phone-img object-fit" alt="mobile-app" />
+                        <div className="absolute sm:bottom-0 bottom-5 left-14 rounded-2xl md:px-4 lg:py-4 px-3 py-2 flex-col bg-white lg:max-w-60 md:max-w-44 sm:max-w-36 max-w-44">
+                            <div className="flex sm:gap-2 gap-1 lg:mb-3 sm:mb-1 mb-2">
+                                <FaStar className="w-4" />
+                                <FaStar className="w-4 " />
+                                <FaStar className="w-4 " />
+                                <FaStar className="w-4" />
+                                <FaStar className="w-4 " />
 
+                            </div>
+                            <p className="text-gray lg:text-sm sm:text-xs text-xs">
+                                4.9 out of 5 Overall Star Rating for All Local Business.
+                            </p>
                         </div>
-                        <p className="text-gray lg:text-sm sm:text-xs text-xs">
-                            4.9 out of 5 Overall Star Rating for All Local Business.
-                        </p>
                     </div>
-                   </div>
                     <div className="sm:hidden grid grid-cols-2 gap-3 mx-auto mb-4">
-                        <a className="px-3 py-2 border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><FaAppStoreIos className="w-6 h-6"  color="blue" /> App Store</a>
-                        <a className="px-3 py-2  border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><BsGooglePlay className="w-6 h-6"  color="blue" /> Google Play</a>
+                        <a className="px-3 py-2 border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><FaAppStoreIos className="w-6 h-6" color="blue" /> App Store</a>
+                        <a className="px-3 py-2  border border-gray-300 rounded-lg flex items-center gap-3 md:text-sm sm:text-sm text-xs"><BsGooglePlay className="w-6 h-6" color="blue" /> Google Play</a>
                     </div>
-                </div>
+                </motion.div>
 
             </div>
-        </header>
+        </motion.header>
     )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,37 +1,62 @@
 import { IoCard } from "react-icons/io5";
 import { LuFlame } from "react-icons/lu";
 import { BiCoffeeTogo } from "react-icons/bi";
-
+import { motion } from "framer-motion";
 
 
 const Main = () => {
+
     return (
         <section className="flex flex-col items-center">
-            <h1 className="font-extrabold sm:my-24 my-16 sm:text-4xl text-3xl text-center mx-auto">Öz zövqünüzə uyğun məhsulları və kofeşopları seçin və sizə uyğun endirimli məhsulları görün.</h1>
-       <div className="grid md:grid-cols-3 grid-cols-1 gap-12 mb-24 px-5">
-        <div className="px-6 py-8 flex flex-col gap-3 shadow-lg  border border-gray-100 rounded-2xl">
-            <h3 className="font-bold text-2xl">Loyalty Card</h3>
-            <p className="text-gray">CoffeeMe den 10 məhsul alana 1 məhsul hədiyyə.</p>
-            <div className="min-h-48 flex items-center justify-center">
-                <IoCard className="sm:w-52 sm:h-52 w-40 h-40 sm:pt-10" color="#214440" />
+            <motion.h1
+             initial={{ opacity: 0, y: 20 }}
+             whileInView={{ opacity: 1, y: 0 }}
+             transition={{ duration: 0.8, ease: "easeOut" }}
+             className="font-extrabold sm:my-24 my-16 sm:text-4xl text-3xl text-center mx-auto">Öz zövqünüzə uyğun məhsulları və kofeşopları seçin və sizə uyğun endirimli məhsulları görün.</motion.h1>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-12 mb-24 px-5">
+                <motion.div
+                    className="px-6 py-8 flex flex-col gap-3 shadow-lg  border border-gray-100 rounded-2xl"
+                    initial={{ scale: 0.7, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.2 }}
+                >
+                    <h3 className="font-bold text-2xl">Loyalty Card</h3>
+                    <p className="text-gray">
+                        CoffeeMe den 10 məhsul alana 1 məhsul hədiyyə.
+                    </p>
+                    <div className="min-h-48 flex items-center justify-center">
+                        <IoCard className="sm:w-52 sm:h-52 w-40 h-40 sm:pt-10" color="#214440" />
+                    </div>
+                </motion.div>
+                <motion.div
+                    className="px-6 py-8 flex flex-col gap-3 shadow-lg  border border-gray-100 rounded-2xl"
+                    initial={{ scale: 0.7, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.4 }}
+                >
+                    <h3 className="font-bold text-2xl">Streaks</h3>
+                    <p className="text-gray">
+                        7 gün ardarda CoffeeMe dən məhsul al və Premiumdan faydalan.
+                    </p>
+                    <div className="sm:min-h-48 min-h-32 flex items-center justify-center">
+                        <LuFlame className="w-40 h-40" color="#214440" />
+                    </div>
+                </motion.div>
+                <motion.div
+                    className="px-6 py-8 flex flex-col gap-3 shadow-lg  border border-gray-100 rounded-2xl"
+                    initial={{ scale: 0.7, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.6 }}
+                >
+                    <h3 className="font-bold text-2xl">Plans</h3>
+                    <p className="text-gray">
+                        CoffeeMe Free və Premium planlarından faydalana bilərsiniz.
+                    </p>
+                    <div className="min-h-48 flex items-center justify-center">
+                        <BiCoffeeTogo className="sm:w-52 sm:h-52 w-40 h-40 sm:pt-10" color="#214440" />
+                    </div>
+                </motion.div>
             </div>
-        </div>
-        <div className="px-6 py-8 flex flex-col gap-3 shadow-lg  border border-gray-100 rounded-2xl">
-            <h3 className="font-bold text-2xl">Streaks</h3>
-            <p className="text-gray">7 gün ardarda CoffeeMe dən məhsul al və Premiumdan faydalan.</p>
-            <div className="sm:min-h-48 min-h-32 flex items-center justify-center">
-                <LuFlame className="w-40 h-40" color="#214440" />
-            </div>
-        </div>
-        <div className="px-6 py-8 flex flex-col gap-3 shadow-lg  border border-gray-100 rounded-2xl">
-            <h3 className="font-bold text-2xl">Plans</h3>
-            <p className="text-gray">CoffeeMe Free və Premium planlarından faydalana bilərsiniz.</p>
-            <div className="min-h-48 flex items-center justify-center">
-                <BiCoffeeTogo className="sm:w-52 sm:h-52 w-40 h-40 sm:pt-10"  color="#214440" />
-            </div>
-        </div>
-      
-       </div>
         </section>
     )
 }
