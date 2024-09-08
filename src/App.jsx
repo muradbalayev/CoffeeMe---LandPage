a8804432b77e87ad1e7a50019c7e704a7f0c0717
Murad Balayev <muradbalazade@gmail.com>
2024-09-08 18:09:53 +0400

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import PricingPlans from './components/PricingPlans'
 function App() {
 
   return (
-    <div className='max-w-[1440px] mx-auto overflow-hidden'>
+    <div className='max-w-[1920px] mx-auto overflow-hidden'>
       <Header />
       <div className="bg-gradient-to-br from-[#f3f4f50f] to-[#54be9539]">
       <About />
